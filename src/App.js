c7e292e1e2351fee0b55ffa11fc9394163429b59
abhay-111 <abhaychauhan11211@gmail.com>
2022-09-18 14:11:14 +0530

    feat : Chara Ui added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import cheffy from './Images/cheffy.jpg';
-import { useEffect, useState } from 'react';
-import './App.css';
-import Card from './Component/Card';
-import axios from 'axios';
+import cheffy from "./Images/cheffy.jpg";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./Component/Card";
+import axios from "axios";
+import { Button, Input, Box } from "@chakra-ui/react";
 function App() {
-  const BASE_URL='https://api.edamam.com/api/recipes/v2?app_id=29d5e4e5&app_key=340a83466c6f827a8c4e3421510293d8&type=public&';
-  const [recipes,setRecipes]=useState([])
-  useEffect(()=>{
-    
-  },[])
-var handleChange=(event)=>{
-    console.log(event.target.value);
-    async function callApi(){
-      const response=await axios({
-        url:BASE_URL+'q='+event.target.value,
-        method:"GET"
-      })
-      setRecipes((ps)=>{
-        ps=response.data.hits;
+  const BASE_URL =
+    "https://api.edamam.com/api/recipes/v2?app_id=29d5e4e5&app_key=340a83466c6f827a8c4e3421510293d8&type=public&";
+  const [recipes, setRecipes] = useState([]);
+  const [search, setsearch] = useState("");
+
+  const handleChange = (e) => {
+    setsearch(e.target.value);
+  };
+
+  const searchRecipe = () => {
+    async function callApi() {
+      const response = await axios({
+        url: BASE_URL + "q=" + search,
+        method: "GET",
+      });
+      setRecipes((ps) => {
+        ps = response.data.hits;
         return ps;
-      })
+      });
     }
     callApi();
-  }
+  };
   return (
-    <div className="App">
-      <input type="text" onChange={handleChange}></input>
-      <div className="container">
-        {
-          recipes.map((recipe)=>{
-            return <Card recipe={recipe}></Card>;
-          })
-        }
-        </div>
-    </div>
-
+    <Box className="App">
+      <Box className="searchBar">
+        <Input type="text" onChange={handleChange}></Input>
+        <span>
+          <Button onClick={searchRecipe}>Search</Button>
+        </span>
+      </Box>
+      <Box className="container">
+        {recipes.map((recipe) => {
+          return <Card recipe={recipe}></Card>;
+        })}
+      </Box>
+    </Box>
   );
 }
 
--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import cheffy from '../Images/cheffy.jpg';
-import './Card.css';
+import { Box, Button, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import cheffy from "../Images/cheffy.jpg";
+import "./Card.css";
 
 function Card(props) {
   // console.log(props.recipe.recipe.label);
+  const gotoRecipePage = () => {
+    window.open(props.recipe.recipe.shareAs);
+  };
+  var calorie = props.recipe.recipe.calories.toString().split(".")[0];
   return (
-    <div className="card">
-          <div className="card-header">
-            <img src={props.recipe ? props.recipe.recipe.image : ''} />
-          </div>
-          <div className="card-body">
-            <h2 className='card-body-header'>{props.recipe ? props.recipe.recipe.label : 'Some Recipe'}</h2>
-            <p>sdfbdsjfsdfnlsdf</p>
-          </div>
-        </div>
-  )
+    <Box boxShadow={"xl"} backgroundColor={"#00B5D8"} className="card">
+      <Box className="card-header">
+        <Image
+          padding={"0px 8px"}
+          src={props.recipe ? props.recipe.recipe.image : ""}
+        />
+      </Box>
+      <Box className="card-body">
+        <Text fontSize={"lg"} fontWeight={"800"} className="card-body-header">
+          {props.recipe
+            ? props.recipe.recipe.label.length > 20
+              ? props.recipe.recipe.label.substring(0, 20) + "..."
+              : props.recipe.recipe.label
+            : "Some Recipe"}
+        </Text>
+        <Text>Calories :- {calorie}</Text>
+        <Button onClick={gotoRecipePage} mt={"5"} size="xs">
+          View Recipe
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default Card;
